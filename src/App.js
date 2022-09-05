e3d6ad18d9b9baafaa2395cffcee324343738062
AnnieTheCatMom <anna.samoilenko1221@gmail.com>
2022-09-05 22:01:39 +0200

    miney

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
 function App() {
+  const [x, setX] = React.useState(null);
+
+  React.useEffect(() => {
+    console.log("Trying to fetch data..");
+    fetch("/cat")
+      .then((res) => res.json())
+      .then((x) => {
+        setX(x.res);
+      });
+    console.log(x);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p> {!x ? "Loading..." : x} </p>
     </div>
   );
 }
